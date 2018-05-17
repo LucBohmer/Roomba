c9eb0fef008bab7af9a7020fb4347852db044349
juliandekievid <38525081+juliandekievid@users.noreply.github.com>
2018-05-17 11:10:48 +0200

    Update Main.cpp

--- roombaMQTT/Main.cpp
+++ roombaMQTT/Main.cpp
@@ -1,1 +1,16 @@
+ /* Roomba project 
+  * Ivo, Lars Luc, Julian
+  *
+  *
+  */
+
+#include AppInfo.h
+
+int main(void){
+
+  DisplayAppInfo();     // Display print application name and version.
+
+return 0;
+
+}
 
