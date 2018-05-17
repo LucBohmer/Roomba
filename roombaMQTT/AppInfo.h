ee9f997af0b8f5f08c4dd2b55a9768b823e99152
LucBohmer <38524913+LucBohmer@users.noreply.github.com>
2018-05-17 11:11:17 +0200

    Update AppInfo.h

--- roombaMQTT/AppInfo.h
+++ roombaMQTT/AppInfo.h
@@ -1,1 +1,14 @@
+#ifndef AppInfo_ H_  /* Include guard */
+#define AppInfo_H_
 
+int AppVersion = 0.0.1;
+string AppName = "Roomba";
+
+void DisplayAppInfo(void){
+ 
+ std::cout<<"Version":<<AppVersion<<std::endl;
+ std::cout<<"Name:"<<AppName<<std::endl;
+}
+
+
+#endif // AppInfo_H_
