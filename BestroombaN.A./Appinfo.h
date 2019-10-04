f7d05aa4054ff9f3c7e1f12157d731c54eccd614
luc bohmer <r.bohmer3@gmail.com>
2019-10-04 11:30:38 +0200

    updated appinfo.h

--- BestroombaN.A./Appinfo.h
+++ BestroombaN.A./Appinfo.h
@@ -1,12 +1,11 @@
 #ifndef APPINFO_H
 #define APPINFO_H
 
-#define APPNAME "BestroombaN.A."
+#define APPNAME "Aspirateur"
 
-#define MAJOR_VERSION "0"
-#define MINOR_VERSION "0"
-#define REVISION_VERSION "1"
-#define VERSION MAJOR_VERSION "." MINOR_VERSION "." REVISION_VERSION
+#define MAJOR_VERSION "2"
+#define MINOR_VERSION "1"
+#define VERSION MAJOR_VERSION "." MINOR_VERSION
 #define APPNAME_VERSION APPNAME " v" VERSION
 
 #endif
