f685f936b078449590990159600d2c042297fbc0
juliandekievid <38525081+juliandekievid@users.noreply.github.com>
2019-10-04 14:00:02 +0200

    MQTT
    added standard mqtt library

---
+++ RoombaMQTT/RoombaMQTTClient.h
@@ -0,0 +1,63 @@
+#ifndef ROOMBAMQTTCLIENT_H
+#define ROOMBAMQTTCLIENT_H
+
+#include "../_libMQTT/CommandProcessor.h"
+#include "../_libUtils/ParLoop.h"
+#include "../_libSenseHAT/SenseHAT.h"
+#include "json.hpp"
+
+using json = nlohmann::json;
+
+using parameters_t = std::vector<std::string>;
+
+class MQTTClient : public CommandProcessor
+{
+public:
+  MQTTClient(const std::string &appname,
+             const std::string &clientname,
+             const std::string &host,
+             int port);
+  MQTTClient() = delete;
+  MQTTClient(const MQTTClient &other) = delete;
+  MQTTClient &operator=(const MQTTClient &other) = delete;
+  virtual ~MQTTClient();
+
+  SenseHAT senseHAT_;
+
+private:
+   // Data examples, not in json format.
+   double pi_;
+   bool happy_;
+   std::string name_;
+   std::vector<int> list_;
+   // json is a first-class data type.
+  json jsonData_;
+
+  void data2json();
+
+protected:
+  const std::string mqttID_;
+  int x_;
+  int y_;
+  const int min_x_ = 0;
+  const int min_y_ = 0;
+  const int max_x_ = 7;
+  const int max_y_ = 7;
+
+  ParLoop publishSensorData_;
+  ParLoop heartbeatPar_;
+
+  // SensorHAT LED matrix display
+  void clear(const parameters_t &commandParameters);
+  void blank(const parameters_t &commandParameters);
+  void invert(const parameters_t &commandParameters);
+  void setPixel(const parameters_t &commandParameters);
+  void getPixel(const parameters_t &commandParameters);
+  void invertPixel(const parameters_t &commandParameters);
+  void cylonPar(const parameters_t &commandParameters);
+  void cylon(const parameters_t &commandParameters);
+  void sendHeartbeat();
+  void handleSensorData();
+};
+
+#endif
---
+++ RoombaMQTT/main.cpp
@@ -0,0 +1,101 @@
+
+#include "AppInfo.h"
+#include "../_libMQTT/CommandProcessor.h"
+#include "../_libMQTT/MQTTconfig.h"
+#include "../_libSenseHAT/SenseHAT.h"
+#include "RandomWalk.h"
+#include "RoombaMQTTclient.h"
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <csignal>
+
+using namespace std;
+
+volatile sig_atomic_t receivedSIGINT{false};
+
+void handleSIGINT(int /* s */)
+{
+   receivedSIGINT = true;
+}
+
+int main(int argc,char *argv[])
+{
+   int majorMosquitto{0};
+   int minorMosquitto{0};
+   int revisionMosquitto{0};
+
+   string mqttBroker{MQTT_LOCAL_BROKER};
+   int mqttBrokerPort{MQTT_LOCAL_BROKER_PORT};
+
+   switch (argc)
+   {
+   case 1:
+      // Using MQTT_LOCAL_BROKER and MQTT_LOCAL_BROKER_PORT
+      break;
+   case 2:
+      // Using MQTT_LOCAL_BROKER_PORT
+      mqttBroker = string(argv[1]);
+      break;
+   case 3:
+      mqttBroker = string(argv[1]);
+      mqttBrokerPort = stoi(argv[2]);
+      break;
+   default:
+      cerr << endl
+           << "ERROR command line arguments:\n"
+              "\tsenseHAT\n"
+              "\tsenseHAT <URL broker>\n"
+              "\tsenseHAT <URL broker> <broker port>\n"
+           << endl;
+      exit(EXIT_FAILURE);
+   }
+
+	try
+   {
+	   signal(SIGINT, handleSIGINT);
+
+      //cout << "-- MQTT application: " << APPNAME_VERSION << "  ";
+      mosqpp::lib_init();
+      mosqpp::lib_version(&majorMosquitto, &minorMosquitto, &revisionMosquitto);
+      cout << "uses Mosquitto lib version "
+           << majorMosquitto
+           << '.' << minorMosquitto
+           << '.' << revisionMosquitto << endl;
+
+	   MQTTClient mqttSenseHAT("MSH", "msh", mqttBroker, mqttBrokerPort);
+		mqttSenseHAT.publishInfo("main()",
+                               "--------- Hello " "APPNAME_VERSION" " ---------");
+      //RandomWalk rw{mqttSenseHAT.senseHAT_.leds};
+
+		// Checking rc for reconnection, 'clients' is an initializer_list
+	   auto clients = {static_cast<mosqpp::mosquittopp*>(&mqttSenseHAT)};
+	   cout << "-- MQTT and SenseHAT are ready" << endl;
+	   while (!receivedSIGINT)
+	   {
+		   for (auto client: clients)
+		   {
+			   int rc = client->loop();
+			   if (rc)
+			   {
+				   cerr << "-- MQTT reconnect" << endl;
+				   client->reconnect();
+			   }
+		   }
+	   }
+   }
+   catch(exception& e)
+   {
+	   cerr << "-- Exception " << e.what() << endl;
+   }
+   catch(...)
+   {
+	   cerr << "-- UNKNOWN EXCEPTION\n";
+   }
+
+   cout << "\n-- MQTT application: " << "APPNAME_VERSION" << " stopped"
+	     << endl << endl;
+   mosqpp::lib_cleanup();
+   return 0;
+}
