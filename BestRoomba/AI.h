dc2c27d6c2d81193e939d7ae482ec1dd34977baa
juliandekievid <38525081+juliandekievid@users.noreply.github.com>
2019-10-04 12:31:10 +0200

    Change running programcode

---
+++ BestRoomba/AI.h
@@ -0,0 +1,19 @@
+#ifndef AI_H
+#define AI_H
+
+#include "../roombaIO/ioHandler.h"
+#include <chrono>
+#include <thread>
+
+/*!
+* \brief AI holds the cleaning stratagy. holds diffecent programms 
+*/
+
+class AI{
+
+    
+};
+
+
+
+#endif
