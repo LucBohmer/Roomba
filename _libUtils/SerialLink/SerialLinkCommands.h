74fc0dfcb22272e0fbbebc12181576c06707af03
IvoEbbers <ijw.ebbers@outlook.com>
2019-11-08 11:32:25 +0100

    added the serial commands

--- _libUtils/SerialLink/SerialLinkCommands.h
+++ _libUtils/SerialLink/SerialLinkCommands.h
@@ -1,6 +1,71 @@
-#ifndef SERIALLINKAS_H
-#define SERIALLINKAS_H
+#ifndef SERIALLINKCOMMANDS_H
+#define SERIALLINKCOMMANDS_H
 #include "SerialLink.h"
-#include "InterfaceConfig.h"
+#include "OpenInterfaceConfig.h"
 
+/*!
+*\brief SerialLink for the BestRoomba HEADER file.
+*\brief Contains default baudrate and different functions.
+*\brief Functions: start_safe, Drive, Spot, Clean, Vacuum.
+*\brief Sensors: isCollision, ButtonSpot.
+*\param value
+*/
+class SerialLinkRoomba
+{
+  private:
+  SerialLink sl{"/dev/ttyUSB0", static_cast<unsigned int>(Baud::ROOMBA_DEFAULT)};
+
+  public:
+    SerialLinkRoomba() {}
+
+///----------------------- FUNCTIONS -----------------------------
+    void start_safe()
+    {
+        sl.write(startSafe());
+    }
+
+    void Drive(int speedL, int speedR)
+    {
+        sl.write(driveDirect(speedR, speedL));
+    }
+
+    void Spot()
+    {
+        sl.write({134});            /// Spot ON
+    }
+
+    void Clean()
+    {
+        sl.write({135});            /// Clean ON
+    }
+
+    void Vacuum(bool state)
+    {
+        if (state)
+        {
+            sl.write({138, 7});     /// Vacuum ON
+        }
+        else
+        {
+            sl.write({138, 0});     /// Vacuum OFF
+        }        
+    }
+
+///----------------------- SENSORS ------------------------------
+    bool isCollision()
+    {
+        return (sl.writeRead({142, 7}, 2).at(1) >> 0) & 0x1;
+    }
+
+    bool ButtonSpot()
+    {
+        return (sl.writeRead({142, 2}, 2).at(1) >> 0) & 0X2;
+    }
+
+///------------------------- LEDS --------------------------------
+    void LedSpotGreen()
+    {
+        sl.write({139, 2, 0, 255});
+    }
+};
 #endif
