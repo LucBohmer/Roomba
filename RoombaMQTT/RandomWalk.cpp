f598a1aa87d5714ff7f5313dc9d4910aa5c9ce6d
Lars Hunneman <lars.hunneman@inspiro.nl>
2021-03-31 16:45:58 +0200

    randomwalk + makefile

---
+++ RoombaMQTT/RandomWalk.cpp
@@ -0,0 +1,34 @@
+#include "RandomWalk.h"
+
+std::mutex RandomWalk::mtx_;
+
+RandomWalk::RandomWalk(LedMatrix& leds,
+                       const Pixel& pixel,
+                       int walkStepSeconds):
+   rdev_{},
+   reng_{rdev_()},
+   uniformRandom_{-1, 1},
+   leds_{},
+   x_{4},
+   y_{4},
+   pixel_{pixel},
+   pl_{std::bind(&RandomWalk::randomWalk, this), walkStepSeconds}
+{}
+
+void RandomWalk::randomWalk()
+{
+   int xPrevious_ = x_;
+   int yPrevious_ = y_;
+
+   std::lock_guard<std::mutex> lgLeds(mtx_);
+   do
+   {
+      x_ = std::min(std::max(LedMatrix::MIN_X, x_ + uniformRandom_(reng_)),
+                    LedMatrix::MAX_X);
+      y_ = std::min(std::max(LedMatrix::MIN_Y, y_ + uniformRandom_(reng_)),
+                    LedMatrix::MAX_Y);
+   } while (leds_.getPixel(x_, y_) != Pixel::BLANK);
+
+   leds_.setPixel(xPrevious_, yPrevious_, Pixel::BLANK);
+   leds_.setPixel(x_, y_, pixel_);
+}
--- RoombaMQTT/RandomWalk.h
+++ RoombaMQTT/RandomWalk.h
@@ -5,47 +5,33 @@
 #include "ParLoop.h"
 #include "Pixel.h"
 
+/// @todo Remove unused include files in the interface.
 #include <algorithm>
 #include <functional>
+#include <mutex>
 #include <random>
 
+/// Active class, uses ParLoop class for executing the random walk of one pixel
+/// on the SenseHAT device #leds_. The function #randomWalk is thread safe,
+/// using a shared mutex object #mtx_.
 class RandomWalk
 {
 public:
-   RandomWalk(LedMatrix& leds):
-      rdev_{},
-      reng_{rdev_()},
-      uniformRandom_{-1, 1},
-      leds_{},
-      x_{1},
-      y_{2},
-      xPrevious_{1},
-      yPrevious_{2},
-      pixelPrevious_{leds_.getPixel(x_, y_)},
-      pl_{std::bind(&RandomWalk::randomWalk, this), 1}
-   {}
-   void randomWalk()
-   {
-      leds_.setPixel(xPrevious_, yPrevious_, pixelPrevious_);
-      x_ = std::min(std::max(LedMatrix::MIN_X, x_ + uniformRandom_(reng_)),
-                    LedMatrix::MAX_X);
-      y_ = std::min(std::max(LedMatrix::MIN_Y, y_ + uniformRandom_(reng_)),
-                    LedMatrix::MAX_Y);
-      pixelPrevious_ = leds_.getPixel(x_, y_);
-      xPrevious_ = x_;
-      yPrevious_ = y_;
-      leds_.setPixel(x_, y_, Pixel(30, 190, 100));
-   }
+   RandomWalk(LedMatrix& leds, const Pixel& pixel, int walkStepSeconds);
+   RandomWalk(const RandomWalk& other) = delete;
+   RandomWalk& operator=(const RandomWalk& other) = delete;
+   void randomWalk();
+
 private:
+   static std::mutex mtx_;
+
    std::random_device rdev_;
    std::default_random_engine reng_;
    std::uniform_int_distribution<int> uniformRandom_;
    LedMatrix leds_;
    int x_;
    int y_;
-   int xPrevious_;
-   int yPrevious_;
-   Pixel pixelPrevious_;
+   Pixel pixel_;
    ParLoop pl_;
 };
 
